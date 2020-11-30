918e5a2212a1d8a07a1f04f9d7ccc2b3d379357f
rudranil roy <73646284+rudranilTHEGREAT10@users.noreply.github.com>
2020-11-30 17:46:55 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,96 @@
+class Ground{
+    constructor(x,y,width,height){
+    
+        var Ground_options={
+            isStatic:true
+        }
+    this.body=Bodies.rectangle(x,y,width,height,Ground_options)
+    this.width=width;
+    this.height=height
+    World.add(world,this.body)
+    
+    
+    
+    
+    
+    
+    
+    }
+    display(){
+    var pos=this.body.position
+    rectMode(CENTER)
+    fill(255)
+    rect(pos.x,pos.y,this.width,this.height)
+    
+    
+    }
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
